80385d83ef9b38cea05856a3276f092162ebc1af
Karen <najafzadehkaren@gmail.com>
2024-02-11 18:17:57 +0330

    registered manager app in admin

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['name','balance','user']
+    search_fields = ['user']
+    list_editable = ['balance']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','balance','user']
+    search_fields = ['user']
+    list_editable = ['balance']
+
+@admin.register(AssetLiability)
+class AssetLiabilityAdmin(admin.ModelAdmin):
+    list_display = ['name','type','user','value','period','account']
+    search_fields = ['user']
+    list_filter = ['type']
+    list_editable = ['value']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['type','user','value','account','category','date']
+    search_fields = ['user']
+    list_filter = ['type']
+    list_editable = ['value']
